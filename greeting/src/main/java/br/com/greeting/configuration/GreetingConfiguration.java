bc5c09cc66365f7c73e691994678dc329f8e76f7
Augusto Mentz <augustohmn.tech@gmail.com>
2021-09-14 17:04:13 -0300

    First commit, add greeting microservice

---
+++ greeting/src/main/java/br/com/greeting/configuration/GreetingConfiguration.java
@@ -0,0 +1,29 @@
+package br.com.greeting.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("greeting-app")
+public class GreetingConfiguration {
+    private String greeting;
+    private String defaultValue;
+
+    public GreetingConfiguration() {}
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+}
---
+++ greeting/src/main/java/br/com/greeting/model/Greeting.java
@@ -0,0 +1,19 @@
+package br.com.greeting.model;
+
+public class Greeting {
+    private final Long id;
+    private final String content;
+
+    public Greeting(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
