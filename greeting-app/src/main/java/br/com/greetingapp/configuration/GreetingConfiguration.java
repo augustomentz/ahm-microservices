c7ce2af0a54eacc5cc67cc6c8cda30e16f146d11
Augusto Mentz <augustohmn.tech@gmail.com>
2021-09-15 16:19:42 -0300

    wip: end of spring cloud configuration

--- greeting-app/src/main/java/br/com/greetingapp/configuration/GreetingConfiguration.java
+++ greeting-app/src/main/java/br/com/greetingapp/configuration/GreetingConfiguration.java
@@ -1,9 +1,11 @@
 package br.com.greeting.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 @ConfigurationProperties("greeting-app")
 public class GreetingConfiguration {
     private String greeting;
