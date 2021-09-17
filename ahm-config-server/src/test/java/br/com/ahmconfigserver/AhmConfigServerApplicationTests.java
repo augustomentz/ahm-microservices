e3b7b9f393d7b00e1c23fbea19ec542890be0a49
Augusto Mentz <augustohmn.tech@gmail.com>
2021-09-17 11:41:23 -0300

    wip: change config server name

---
+++ ahm-config-server/src/test/java/br/com/ahmconfigserver/AhmConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ahmconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhmConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
